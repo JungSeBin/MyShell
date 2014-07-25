84c9f64d74f7f4829a9d4490e473aba12b6f018f
sebin <tpqls5144@naver.com>
2014-07-25 16:53:10 +0900

    first commit

---
+++ perfmon/perfmon.cpp
@@ -0,0 +1,80 @@
+// perfmon.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <string.h>
+#pragma  warning(disable:4996)
+
+#define MAX_STR_LEN 256
+#define CMD_TOKEN_NUM 10
+
+TCHAR ERROR_CMD[]
+= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다.\n");
+
+int CmdProcessing();
+TCHAR* StrLower(TCHAR*);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//한글 입력을 가능케 하기 위해.
+	_tsetlocale(LC_ALL, _T("Korean"));
+
+	DWORD isExit;
+	while (1)
+	{
+		isExit = CmdProcessing();
+		if (isExit == TRUE)
+		{
+			_fputts(_T("명령어 처리를 종료합니다.\n"), stdout);
+			break;
+		}
+	}
+	return 0;
+}
+
+TCHAR cmdString[MAX_STR_LEN];
+TCHAR cmdTokenList[CMD_TOKEN_NUM][MAX_STR_LEN];
+TCHAR seps[] = _T(",\t\n");
+
+int CmdProcessing()
+{
+	_fputts(_T("My shell>> "), stdout);
+	_getts(cmdString);
+
+	TCHAR* token = _tcstok(cmdString, seps);
+
+	int tokenNum = 0;
+	while (token != NULL)
+	{
+		_tcscpy(
+			cmdTokenList[tokenNum++], StrLower(token)
+			);
+		token = _tcstok(NULL, seps);
+	}
+
+	if (!_tcscmp(cmdTokenList[0], _T("exit")))
+	{
+		return TRUE;
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("pwd")))
+	{
+		TCHAR buffer[MAX_STR_LEN] = { 0, };
+		GetCurrentDirectory(MAX_STR_LEN, buffer);
+		_putts(buffer);
+	}
+	else
+		_putts(cmdTokenList[0]);
+	return 0;
+}
+
+TCHAR* StrLower(TCHAR* input)
+{
+	for (int i = 0; input[i]; i++)
+	{
+		input[i] = tolower(input[i]);
+	}
+	return input;
+}
