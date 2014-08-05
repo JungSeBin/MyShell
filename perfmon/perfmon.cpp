9d4d1d246d6d6cf6b29fd8cf93cbeac29dab74fc
sebin <tpqls5144@naver.com>
2014-08-05 13:38:58 +0900

    echo, start 명령어 추가

--- perfmon/perfmon.cpp
+++ perfmon/perfmon.cpp
@@ -14,37 +14,48 @@
 //TCHAR ERROR_CMD[]
 //= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다.\n");
 
+TCHAR* cmdName = NULL;
+TCHAR cmdString[MAX_STR_LEN] = { 0, };
+TCHAR cmdTokenList[CMD_TOKEN_NUM][MAX_STR_LEN];
+TCHAR seps[] = _T(" ,\t\n");
+
 int CmdProcessing();
 TCHAR* StrLower(TCHAR*);
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	DWORD isExit = FALSE;
 	//한글 입력을 가능케 하기 위해.
 	_tsetlocale(LC_ALL, _T("Korean"));
-
-	DWORD isExit;
-	while (1)
+	cmdName = argv[0];
+	if (argc > 1)
 	{
-		isExit = CmdProcessing();
-		if (isExit == TRUE)
+		for (int i = 1; i < argc; i++)
 		{
-			_fputts(_T("명령어 처리를 종료합니다.\n"), stdout);
-			break;
+			_tcscat(cmdString, argv[i]);
+			_tcscat(cmdString, _T(" "));
 		}
+		isExit = CmdProcessing();
 	}
+
+	
+	while (isExit == FALSE)
+	{
+		_fputts(_T("My shell>> "), stdout);
+		_getts(cmdString);
+		isExit = CmdProcessing();
+	}
+	_fputts(_T("명령어 처리를 종료합니다.\n"), stdout);
 	return 0;
 }
 
-TCHAR cmdString[MAX_STR_LEN];
-TCHAR cmdTokenList[CMD_TOKEN_NUM][MAX_STR_LEN];
-TCHAR seps[] = _T(",\t\n");
 
 int CmdProcessing()
 {
-	_fputts(_T("My shell>> "), stdout);
-	_getts(cmdString);
+	TCHAR cmdBuffer[MAX_STR_LEN] = { 0, };
+	
+	_tcscpy(cmdBuffer, cmdString);
 
 	TCHAR* token = _tcstok(cmdString, seps);
 
@@ -67,6 +78,32 @@ int CmdProcessing()
 		GetCurrentDirectory(MAX_STR_LEN, buffer);
 		_putts(buffer);
 	}
+	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
+	{
+		_putts(cmdBuffer + 5);
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("start")))
+	{
+		STARTUPINFO si = { 0, };
+		PROCESS_INFORMATION pi;
+		TCHAR startString[MAX_STR_LEN] = { 0, };
+
+		_tcscat(startString, cmdName);
+		_tcscat(startString, cmdBuffer + 5);
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		ZeroMemory(&pi, sizeof(pi));
+
+		if (CreateProcess(NULL, startString, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+		{
+			CloseHandle(pi.hProcess);
+			CloseHandle(pi.hThread);
+		}
+		else
+		{
+			_putts(_T("Error"));
+		}
+	}
 	else
 	{
 		STARTUPINFO si = { 0, };
@@ -76,9 +113,9 @@ int CmdProcessing()
 		si.cb = sizeof(si);
 		ZeroMemory(&pi, sizeof(pi));
 
-		if (!CreateProcess(NULL, cmdString, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi))
+		if (!CreateProcess(NULL, cmdBuffer, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi))
 		{
-			_putts(cmdTokenList[0]);
+			_putts(cmdBuffer);
 		}
 		else
 		{
